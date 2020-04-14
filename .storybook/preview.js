d40ad5a9e6d07c373c9917061a80ab4d2d576e35
David Anderson <danderson@crh.io>
2020-04-14 15:12:02 -0400

    Remove fontawesome dep

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,1 @@
 import '../src/assets/styles/index.scss';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { far } from '@fortawesome/pro-regular-svg-icons';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
-
-
-library.add(far);
-library.add(fas);
