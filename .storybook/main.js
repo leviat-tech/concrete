47b0c3e6c5f5746f6bc9f8d56ee6d792f6268e61
David Anderson <danderson@crh.io>
2020-04-27 00:44:07 -0400

    Remove unused addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,22 +2,22 @@ module.exports = {
   stories: ['./**/*.stories.(js|jsx|ts|tsx|mdx)'],
   addons: [
     // '@storybook/addon-actions',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        babelOptions: {
-          presets: [
-            [
-              '@vue/cli-plugin-babel/preset',
-              {
-                jsx: false,
-              },
-            ],
-          ],
-        },
-      },
-    },
-    '@storybook/addon-knobs',
+    // {
+    //   name: '@storybook/addon-docs',
+    //   options: {
+    //     babelOptions: {
+    //       presets: [
+    //         [
+    //           '@vue/cli-plugin-babel/preset',
+    //           {
+    //             jsx: false,
+    //           },
+    //         ],
+    //       ],
+    //     },
+    //   },
+    // },
+    // '@storybook/addon-knobs',
     // '@storybook/addon-links',
     '@storybook/addon-storysource',
     // 'storybook-addon-designs',
