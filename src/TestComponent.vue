6575b1c2c3d337ceea7c1af4c52069cb8b072fcf
David Anderson <danderson@crh.io>
2021-09-15 18:07:03 -0400

    Remove extra container div in c-icon

--- src/TestComponent.vue
+++ src/TestComponent.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="tab-div">
     <c-panel-slider title="Primary">
+      <c-toolbar v-model="currentTool" default-tool="select">
+        <c-tool-group>
+          <c-tool name="Undo" tool-id="undo" icon="undo" />
+          <c-tool name="Redo" tool-id="redo" icon="redo" />
+        </c-tool-group>
+        <c-tool-group>
+          <c-tool name="Select" tool-id="select" icon="pointer-outline" />
+          <c-tool name="Pan" tool-id="pan" icon="hand" />
+          <c-tool name="Pen" tool-id="pen" icon="pen" />
+          <c-tool name="Code" tool-id="zoom-to-fit">
+            <zoom-to-fit-icon />
+          </c-tool>
+        </c-tool-group>
+      </c-toolbar>
       Test component.
       <div>
         <c-panel-link link-to="a">Drill Down</c-panel-link>
         <c-panel-link link-to="b">A different menu</c-panel-link>
       </div>
       Some more content.
-      <c-icon class="lg">
+      <c-icon>
         <c-code />
+      </c-icon>
+      <c-icon>
+        <zoom-to-fit-icon />
       </c-icon>
       <c-icon type="pointer-outline" />
       <c-panel panel-id="a" title="Secondary">
@@ -23,7 +40,9 @@
 </template>
 
 <script>
+import { CToolbar, CToolGroup, CTool } from '@/components/Toolbar';
 import { CPanelSlider, CPanel, CPanelLink } from '@/components/PanelSlider';
+import ZoomToFitIcon from '@/assets/icons/zoom-to-fit.svg';
 import NestedComponent from '@/NestedComponent';
 import CIcon from '@/components/Icon';
 import CCode from '@/assets/icons/code.svg';
@@ -32,12 +51,21 @@ import CCode from '@/assets/icons/code.svg';
 export default {
   name: 'TestComponent',
   components: {
+    CToolbar,
+    CToolGroup,
+    CTool,
+    ZoomToFitIcon,
     NestedComponent,
     CPanelSlider,
     CPanel,
     CIcon,
     CPanelLink,
     CCode,
+  },
+  data() {
+    return {
+      currentTool: 'select',
+    };
   },
 };
 </script>
