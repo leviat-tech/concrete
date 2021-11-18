8d74438f930d9f1cceff6f1d97fe433ceae8cb0c
mattbarton44 <mattbarton44@gmail.com>
2021-11-18 11:12:39 +0000

    update SI units

--- src/utils/units.js
+++ src/utils/units.js
@@ -18,7 +18,32 @@ const conversions = {
   deg: {
     rad: (rad) => rad.times(180.0).div(Math.PI),
   },
+  'N/m': {
+    'kN/m': (knm) => knm.times(1000),
+  },
+  'kN/m': {
+    'N/m': (nm) => nm.div(1000),
+  },
+  N: {
+    kN: (kN) => kN.times(1000),
+  },
+  kN: {
+    N: (N) => N.div(1000),
+  },
+  'N/m³': {
+    'kN/m³': (knm3) => knm3.times(1000),
+  },
+  'kN/m³': {
+    'N/m³': (nm3) => nm3.div(1000),
+  },
+  'kN/m²': {
+    'N/m²': (nm2) => nm2.div(1000),
+  },
+  'N/m²': {
+    'kN/m²': (knm2) => knm2.times(1000),
+  },
 };
+
 
 const SI = {
   m: 'm',
@@ -27,6 +52,13 @@ const SI = {
   rad: 'rad',
   deg: 'rad',
   'N/m': 'N/m',
+  'kN/m': 'N/m',
+  N: 'N',
+  kN: 'N',
+  'kN/m³': 'N/m³',
+  'kN/m²': 'N/m²',
+  K: 'K',
+  'W/m*K': 'W/m*K',
 };
 
 const aliases = {
