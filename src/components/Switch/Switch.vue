65eca84a2478869da41a799579082dfb6a611634
mattbarton44 <mattbarton44@gmail.com>
2022-07-12 16:39:26 +0100

    chore: replace icons in switch with heroicons

--- src/components/Switch/Switch.vue
+++ src/components/Switch/Switch.vue
@@ -13,14 +13,10 @@
       `h-${sized-1} w-${sized-1}`,
     ]">
       <span v-if="displayIcons" :class="[enabled ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200', 'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity']" aria-hidden="true">
-        <svg :class="[`h-${sized/2} w-${sized/2} text-gray-400`]" fill="none" viewBox="0 0 12 12">
-          <path d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
+        <XIcon :class="[`h-${sized/2} w-${sized/2} text-gray-400`]" />
       </span>
       <span v-if="displayIcons" :class="[enabled ? 'opacity-100 ease-in duration-200' : 'opacity-0 ease-out duration-100', 'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity']" aria-hidden="true">
-        <svg :class="[`h-${sized/2} w-${sized/2} text-${color}`]" fill="currentColor" viewBox="0 0 12 12">
-          <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-        </svg>
+        <CheckIcon :class="[`h-${sized/2} w-${sized/2} text-${color}`]" />
       </span>
     </span>
   </Switch>
@@ -29,6 +25,7 @@
 
 <script setup>
   import { Switch } from '@headlessui/vue';
+  import { CheckIcon, XIcon } from '@heroicons/vue/solid';
   import { computed } from 'vue';
 
   const props = defineProps({
