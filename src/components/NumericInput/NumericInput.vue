6475bdb14ba74938aa7d0455d0b4d070f673378c
mattbarton44 <mattbarton44@gmail.com>
2022-07-15 17:11:37 +0100

    feat: implement numeric input

--- src/components/NumericInput/NumericInput.vue
+++ src/components/NumericInput/NumericInput.vue
@@ -1,31 +1,47 @@
 <template>
-  <div class="flex w-full">
+  <div class="flex w-full relative">
     <slot name="prefix" class="z-10"/>
     <input
       ref="inputRef"
       
       :id="id"
       v-model="value"
-      type="text"
-      :class="['relative truncate z-20 w-full border text-left pl-3 pr-3 focus:outline-none focus:ring-1 focus:border-indigo-light focus:ring-indigo-light', sizeClass, colorClass, disabledClass, cursorClass, bgColor ]"
+      type="number"
+      :class="[
+        'block truncate z-20 w-full border text-left px-3 focus:outline-none focus:ring-1 focus:border-indigo-light focus:ring-indigo-light',
+        sizeClass, colorClass, disabledClass, cursorClass, bgColor
+      ]"
       :placeholder="placeholder"
       :disabled="disabled"
       :readonly="readOnly"
+      :step="step"
+      :min="minimum"
+      :max="maximum"
       
       @keydown.enter="$emit('enter')"
       @blur="$emit('blur')"
     >
-    <slot name="suffix" class="z-10" />
+    <div :class="['absolute inset-y-0 z-30 right-0 flex items-center pointer-events-none', paddingClass]">
+      <!-- unit -->
+      <div v-if="to || unit" class="unit">{{ to || unit }}</div>
+    </div>
+    <slot name="suffix" class="z-10">
+    </slot>
+
+
+
   </div>
 </template>
 
 <script setup>
   
+  import Big from 'big.js';
+  import { convert, convertFromSI, convertToSI, isNumber } from '../..//utils/units';
   import { computed, ref } from 'vue';
 
   const props = defineProps({
     id: { type: String, default: null },
-    modelValue: String,
+    modelValue: Number,
     color: {
       type: String,
       default: 'default',
@@ -42,18 +58,53 @@
     readOnly: { type: Boolean, default: false },
     placeholder: { type: String, default: '' },
     transparent: { type: Boolean, default: false },
+
+    precision: { type: Number, default: null },
+    unit: { type: String, default: null },
+    maximum: { type: Number, default: null },
+    minimum: { type: Number, default: null },
+    step: { type: Number, default: null },
+    to: { type: String, default: null },
+    from: { type: String, default: null },
+
+
   });
 
   const emit = defineEmits(['update:modelValue', 'enter', 'blur']);
 
+  let localValue = ref(null);
+
   const value = computed({
     get() {
-      return props.modelValue
+      return convertToDisplayValue(props.modelValue);
     },
     set(value) {
-      emit('update:modelValue', value)
+      localValue.value = convertToDisplayValue(value);
+      emit('update:modelValue', convertFromDisplayValue(value))
     }
   });
+
+  const convertToDisplayValue = (v) => {
+    if (v === null || v === '') return null;
+    let value = null;
+    if (!isNumber(v)) value = v;
+    if (props.unit) value = convertFromSI(v, props.unit);
+    if (props.from && props.to) value = convert(v, props.from, props.to);
+    if (value === null) value = Number(v);
+    return (props.precision === null) ? value : parseFloat(value.toFixed(props.precision), 10);
+  };
+
+  const convertFromDisplayValue = (v) => {
+    if (v === null || v === '') return null;
+    let value = null;
+    if (!isNumber(v)) return v;
+    if (props.unit) value = convertToSI(Number(v), props.unit);
+    if (props.from && props.to) value = convert(Number(v), props.to, props.from);
+    if (value === null) value = Number(v);
+    return value;
+  };
+
+
 
   const sizeClass = {
     xs: 'h-6 text-xs py-0.5',
@@ -65,6 +116,8 @@
   const cursorClass = (props.disabled) ? 'cursor-not-allowed' : 'cursor-text';
   
   const bgColor =  (props.transparent) ? 'bg-transparent' : 'bg-white';
+
+  const paddingClass = (props.readOnly || props.disabled) ? 'pr-3' : 'pr-8';
 
   const colorClass = computed(() => {
     return {
