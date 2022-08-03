050c4fd651cabe42072e5b99d32f0ac6db25699a
mattbarton44 <mattbarton44@gmail.com>
2022-08-03 13:18:36 +0100

    chore: update index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
+
+import CAccordion from './components/Accordion/Accordion.vue';
 import CButton from './components/Button/Button.vue';
-import CSwitch from './components/Switch/Switch.vue';
+import CDraggablePath from './components/DraggablePath/DraggablePath.vue';
+import CDraggablePoint from './components/DraggablePoint/DraggablePoint.vue';
+import CFormElement from './components/FormElement/FormElement.vue';
+import CFormSection from './components/FormSection/FormSection.vue';
+import CFragment from './components/Fragment/Fragment.vue';
 import CIcon from './components/Icon/Icon.vue';
+import CInputAffix from './components/InputAffix/InputAffix.vue';
+import CListbox from './components/Listbox/Listbox.vue';
 import CModal from './components/Modal/Modal.vue';
-import { CToolbar, CToolGroup, CTool } from './components/Toolbar';
+import CNumericInput from './components/NumericInput/NumericInput.vue';
+import CStatusBar from './components/StatusBar/StatusBar.vue';
+import CSwitch from './components/Switch/Switch.vue';
 import CTextInput from './components/TextInput/TextInput.vue';
+import { CToolbar, CToolGroup, CTool } from './components/Toolbar';
+
 
 const allComponents = {
+  CAccordion,
   CButton,
-  CSwitch,
+  CDraggablePath,
+  CDraggablePoint,
+  CFormElement,
+  CFormSection,
+  CFragment,
   CIcon,
+  CInputAffix,
+  CListbox,
+  CModal,
+  CNumericInput,
+  CStatusBar,
+  CSwitch,
+  CTextInput,
   CToolbar,
   CToolGroup,
   CTool,
-  CModal,
-  CTextInput,
 };
 
 export const defaultOptions = {
@@ -43,13 +65,24 @@ export default {
 };
 
 export {
+  CAccordion,
   CButton,
-  CSwitch,
+  CDraggablePath,
+  CDraggablePoint,
+  CFormElement,
+  CFormSection,
+  CFragment,
   CIcon,
+  CInputAffix,
+  CListbox,
+  CModal,
+  CNumericInput,
+  CStatusBar,
+  CSwitch,
+  CTextInput,
   CToolbar,
   CToolGroup,
-  CModal,
-  CTextInput,
+  CTool,
 }
 
 
