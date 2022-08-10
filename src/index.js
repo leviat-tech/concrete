1a70fb22205f0eac090817251f972101840818c0
robiharid <rh@robx.tech>
2022-08-10 14:11:38 +0100

    Add basic TextArea SFC and create story

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 // Inputs and Form Components
-import CButton from './components/Button/Button.vue';
-import CSwitch from './components/Switch/Switch.vue';
-import CTextInput from './components/TextInput/TextInput.vue';
-import CNumericInput from './components/NumericInput/NumericInput.vue';
-import CListbox from './components/Listbox/Listbox.vue';
-import CCheckbox from './components/Checkbox/Checkbox.vue';
+import CButton from "./components/Button/Button.vue";
+import CSwitch from "./components/Switch/Switch.vue";
+import CTextInput from "./components/TextInput/TextInput.vue";
+import CTextArea from "./components/TextArea/TextArea.vue";
+import CNumericInput from "./components/NumericInput/NumericInput.vue";
+import CListbox from "./components/Listbox/Listbox.vue";
+import CCheckbox from "./components/Checkbox/Checkbox.vue";
 
 // Low Level Components
-import CIcon from './components/Icon/Icon.vue';
-import CFormSection from './components/FormSection/FormSection.vue';
-import CFormElement from './components/FormElement/FormElement.vue';
-import CInputAffix from './components/InputAffix/InputAffix.vue';
+import CIcon from "./components/Icon/Icon.vue";
+import CFormSection from "./components/FormSection/FormSection.vue";
+import CFormElement from "./components/FormElement/FormElement.vue";
+import CInputAffix from "./components/InputAffix/InputAffix.vue";
 
 // High Level Components
-import CModal from './components/Modal/Modal.vue';
-import CAccordion from './components/Accordion/Accordion.vue';
-import CStatusBar from './components/StatusBar/StatusBar.vue';
-import { CToolbar, CToolGroup, CTool } from './components/Toolbar';
+import CModal from "./components/Modal/Modal.vue";
+import CAccordion from "./components/Accordion/Accordion.vue";
+import CStatusBar from "./components/StatusBar/StatusBar.vue";
+import { CToolbar, CToolGroup, CTool } from "./components/Toolbar";
 
-import { CViewportContainer, CViewport } from './components/Viewport';
-import CDraggablePath from './components/DraggablePath/DraggablePath.vue';
-import CDraggablePoint from './components/DraggablePoint/DraggablePoint.vue';
+import { CViewportContainer, CViewport } from "./components/Viewport";
+import CDraggablePath from "./components/DraggablePath/DraggablePath.vue";
+import CDraggablePoint from "./components/DraggablePoint/DraggablePoint.vue";
 
-import { defaultOptions } from './composables/concrete';
-
+import { defaultOptions } from "./composables/concrete";
 
 const allComponents = {
   CAccordion,
@@ -41,29 +41,26 @@ const allComponents = {
   CStatusBar,
   CSwitch,
   CTextInput,
+  CTextArea,
   CTool,
   CToolbar,
   CToolGroup,
   CViewport,
-  CViewportContainer
+  CViewportContainer,
 };
 
-
-
 const install = (app, userOptions = {}) => {
-
   const options = { ...defaultOptions, ...userOptions };
   const { size, components, inputHandler, labelFormatter } = options;
 
-  app.provide('concrete', { size, inputHandler, labelFormatter });
+  app.provide("concrete", { size, inputHandler, labelFormatter });
 
   const componentsToInclude = components || Object.keys(allComponents);
 
   componentsToInclude.forEach((componentName) => {
     const component = allComponents[componentName];
-      app.component(componentName, component);
+    app.component(componentName, component);
   });
-
 };
 
 export default {
@@ -86,9 +83,10 @@ export {
   CStatusBar,
   CSwitch,
   CTextInput,
+  CTextArea,
   CTool,
   CToolbar,
   CToolGroup,
   CViewport,
-  CViewportContainer
-}
+  CViewportContainer,
+};
