ef77df8a3b4134685d54aeb1613ceb18bc250fbb
robiharid <rh@robx.tech>
2022-08-17 10:19:01 +0100

    chore: Clean up script setup unused handlers for Tabs

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -54,22 +54,9 @@
 </template>
 
 <script setup>
-import { computed, ref, inject } from 'vue';
-import { formElementProps } from '../../composables/props.js';
-import { useInputColorClassValue } from '../../composables/styles';
-import {
-  useSizeValue,
-  useStackedValue,
-  useFormElementValue,
-} from '../../composables/forms';
-import { useEventHandler } from '../../composables/events.js';
-import CFormElement from '../FormElement/FormElement.vue';
-import CFragment from '../Fragment/Fragment.vue';
 import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue';
 
 const props = defineProps({
-  ...formElementProps,
-  modelValue: String,
   tabPosition: {
     default: 'top',
     validator(value) {
@@ -77,44 +64,5 @@ const props = defineProps({
       return ['top', 'bottom'].includes(value);
     },
   },
-});
-
-const emit = defineEmits(['update:modelValue', 'enter', 'blur']);
-
-const size = useSizeValue(props.size);
-const stacked = useStackedValue(props.stacked);
-const formElement = useFormElementValue(props.label);
-
-const isDirty = ref(false);
-const inputRef = ref(null);
-
-const value = computed({
-  get() {
-    return props.modelValue;
-  },
-  set(value) {
-    isDirty.value = true;
-    emit('update:modelValue', value);
-  },
-});
-
-const onEnter = useEventHandler('enter', props, emit, value, isDirty);
-const onBlur = useEventHandler('blur', props, emit, value, isDirty);
-const focus = () => inputRef.value.focus();
-const blur = () => inputRef.value.blur();
-defineExpose({ focus, blur });
-
-const sizeClass = {
-  xs: 'h-6 text-xs py-0.5',
-  sm: 'h-8 text-sm py-1',
-  md: 'h-10 text-base py-2',
-  lg: 'h-12 text-lg py-2',
-}[size];
-
-const cursorClass = props.disabled ? 'cursor-not-allowed' : 'cursor-text';
-const bgColor = props.transparent ? 'bg-transparent' : 'bg-white';
-const colorClass = useInputColorClassValue(props.color);
-const disabledClass = computed(() => {
-  return props.disabled && 'opacity-60';
 });
 </script>
