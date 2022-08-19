2c5d26b219d2bb655dedc847ea1b9e82022fbfbb
robiharid <rh@robx.tech>
2022-08-19 16:33:11 +0100

    chore: simplify conditional class on active tab select

--- src/components/Tabs/CTabLabel.vue
+++ src/components/Tabs/CTabLabel.vue
@@ -6,7 +6,7 @@
   <Tab v-slot="{ selected }" as="template">
     <button
       class="w-full items-center justify-around p-4 hover:bg-gray-100"
-      :class="[selected ? 'bg-gray-50 font-bold z-[2]' : '']"
+      :class="{ 'bg-gray-50 font-bold z-[2]': selected }"
     >
       <slot />
     </button>
