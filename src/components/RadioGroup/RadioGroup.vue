e484b70e13c251c33aec38a6ad1e6ea412c56326
Calum Hainsworth <calum.hainsworth@leviat.com>
2022-08-25 09:30:24 +0100

    chore: removed formatting and unused imports

--- src/components/RadioGroup/RadioGroup.vue
+++ src/components/RadioGroup/RadioGroup.vue
@@ -5,33 +5,33 @@
     v-bind="{ id, label, size, color, labelFormatter, message, stacked }"
   >
   <div :class="[ textSizeClass, disabledClass]">
-  <RadioGroup 
-  class="concrete__radiogroup"
-  :class="layoutClass"
-  v-model="value" 
-  ref="inputref"
-  :disabled='disabled'
-  >
-    <RadioGroupOption v-for="option in options" :key="option" :value="option" v-slot="{ checked }">
-      <div class="flex" >
-        <span class="mr-0.5 self-center grow text-right" :class="inputColorClass"> {{ props.formatter ? props.formatter(option) : option }}</span>
-            <svg :class="[heightClass,svgColour]" class="self-center" viewBox="0 0 20 20" >
-                <circle cx="10" cy="10" r="7" fill="none" />
-                <circle cx="10" cy="10" r="5.5" stroke="none" v-if="checked"  />
-            </svg>
-      </div>
-    </RadioGroupOption>
-  </RadioGroup>
+    <RadioGroup 
+    class="concrete__radiogroup"
+    :class="layoutClass"
+    v-model="value" 
+    ref="el"
+    :disabled='disabled'
+    >
+      <RadioGroupOption v-for="option in options" :key="option" :value="option" v-slot="{ checked }">
+        <div class="flex" >
+          <span class="mr-0.5 self-center grow text-right" :class="inputColorClass"> {{ props.formatter ? props.formatter(option) : option }}</span>
+              <svg :class="[heightClass,svgColour]" class="self-center" viewBox="0 0 20 20" >
+                  <circle cx="10" cy="10" r="7" fill="none" />
+                  <circle cx="10" cy="10" r="5.5" stroke="none" v-if="checked"  />
+              </svg>
+        </div>
+      </RadioGroupOption>
+    </RadioGroup>
   </div>
   </component>
 </template>
 
 <script setup>
-import { RadioGroup, RadioGroupLabel, RadioGroupOption } from "@headlessui/vue";
+import { RadioGroup,  RadioGroupOption } from "@headlessui/vue";
 import CFormElement from '../FormElement/FormElement.vue';
 import CFragment from '../Fragment/Fragment.vue';
 import { computed, ref } from "vue";
-import { colorProp, formElementProps, useSizeProp } from '../../composables/props';
+import { formElementProps } from '../../composables/props';
 import { useInputClasses } from '../../composables/styles';
 import {
   useFormElementValue,
@@ -46,15 +46,11 @@ const formElement = useFormElementValue(props.label);
 const props = defineProps({
   ...formElementProps,
   modelValue: [String, Object, Array],
-  options: {
-    type: Array,
-    default(rawProps) {
-      return [];
-    },
-  },
+  options: { type: Array },
   formatter: { type: Function, default: null },
   transparent: { type: Boolean, default: false },
   columns: {type:Number, default:0},
+  onChange: { type: Function, default: null },
 });
 
 const value = computed({
@@ -80,9 +76,7 @@ useRegisterInput(props.id, el);
 const stacked = useStackedValue(props.stacked);
 
 const {
-  mergedSizeClass,
   inputColorClass,
-  bgColorClass,
   disabledClass,
   textSizeClass,
   heightClass,
