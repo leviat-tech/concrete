3bddbd72cc9109063ed66b51dda957ac1fe0b3b4
robiharid <rh@robx.tech>
2022-09-05 12:59:40 +0100

    feat: Implement testing in CSF with props (args)

--- src/components/Tabs/__stories__/Tabs.stories.js
+++ src/components/Tabs/__stories__/Tabs.stories.js
@@ -1,8 +1,13 @@
 import CTabs from '../Tabs.vue';
 import CTabPanel from '../CTabPanel.vue';
+import { userEvent, screen, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
-export const Overview = () => ({
+export const Overview = (args) => ({
   components: { CTabs, CTabPanel },
+  setup() {
+    return args;
+  },
   template: /*html*/ `
   <CTabs :labels="['Tab 1','Tab 2','Tab 3']" class="border"> 
     <template #panels> 
@@ -11,31 +16,34 @@ export const Overview = () => ({
       <CTabPanel>Panel 3</CTabPanel> 
     </template>
   </CTabs>`,
-  play: async (args, canvasElement) => {
-    const canvas = within(canvasElement);
-    // Check that labels array is the same length as the number of visible tabs
-    const tabs = await screen.findByRole('tablist');
-    const numberOfTabs = tabs.children.length;
-    const numberOfLabelsArg = args.labels.length;
-    expect(numberOfTabs).toBe(numberOfLabelsArg);
-    // Check that the all tabs are visible and match the labels prop supplied
-    for (const tab of tabs.children) {
-      const label = tab.innerText;
-      // Check that the tab on screen is visible
-      const htmlTab = await screen.findByText(label);
-      expect(htmlTab).toBe(tab);
-    }
-    // Click on the second tab and assert that panel is displayed
-    await userEvent.click(canvas.getByRole('tab', { name: 'Tab 2' }));
-    await screen.findByText('Panel 2');
-    // Click on the third tab and assert that panel is displayed
-    await userEvent.click(canvas.getByRole('tab', { name: 'Tab 3' }));
-    await screen.findByText('Panel 3');
-    // Click on the first tab and assert that panel is displayed
-    await userEvent.click(canvas.getByRole('tab', { name: 'Tab 1' }));
-    await screen.findByText('Panel 1');
-  },
 });
+Overview.args = {
+  labels: ['Tab 1', 'Tab 2', 'Tab 3'],
+};
+Overview.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Check that labels array is the same length as the number of visible tabs
+  const tabs = await screen.findByRole('tablist');
+  const numberOfTabs = tabs.children.length;
+  const numberOfLabelsArg = args.labels.length;
+  expect(numberOfTabs).toBe(numberOfLabelsArg);
+  // Check that the all tabs are visible and match the labels prop supplied
+  for (const tab of tabs.children) {
+    const label = tab.innerText;
+    // Check that the tab on screen is visible
+    const htmlTab = await screen.findByText(label);
+    expect(htmlTab).toBe(tab);
+  }
+  // Click on the second tab and assert that panel is displayed
+  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 2' }));
+  await screen.findByText('Panel 2');
+  // Click on the third tab and assert that panel is displayed
+  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 3' }));
+  await screen.findByText('Panel 3');
+  // Click on the first tab and assert that panel is displayed
+  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 1' }));
+  await screen.findByText('Panel 1');
+};
 Overview.parameters = {
   docs: {
     source: {
