ca4b4731dd1deefe8b55dab41fb3de46d5e92a1b
robiharid <rh@robx.tech>
2022-09-06 13:06:10 +0100

    feat: implement play composition testing, create base case and apply it to story variations in Tabs

--- src/components/Tabs/__stories__/Tabs.stories.js
+++ src/components/Tabs/__stories__/Tabs.stories.js
@@ -23,33 +23,6 @@ export const Overview = (args) => ({
     </template>
   </CTabs>`,
 });
-Overview.args = {
-  labels: ['Tab 1', 'Tab 2', 'Tab 3'],
-};
-Overview.play = async ({ args, canvasElement }) => {
-  const canvas = within(canvasElement);
-  // Check that labels array is the same length as the number of visible tabs
-  const tabs = await screen.findByRole('tablist');
-  const numberOfTabs = tabs.children.length;
-  const numberOfLabelsArg = args.labels.length;
-  expect(numberOfTabs).toBe(numberOfLabelsArg);
-  // Check that the all tabs are visible and match the labels prop supplied
-  for (const tab of tabs.children) {
-    const label = tab.innerText;
-    // Check that the tab on screen is visible
-    const htmlTab = await screen.findByText(label);
-    expect(htmlTab).toBe(tab);
-  }
-  // Click on the second tab and assert that panel is displayed
-  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 2' }));
-  await screen.findByText('Panel 2');
-  // Click on the third tab and assert that panel is displayed
-  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 3' }));
-  await screen.findByText('Panel 3');
-  // Click on the first tab and assert that panel is displayed
-  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 1' }));
-  await screen.findByText('Panel 1');
-};
 
 export const LabelSlots = (args) => ({
   components: { CTabs, CTabLabel, CTabPanel },
@@ -70,3 +43,40 @@ export const LabelSlots = (args) => ({
     </template>
   </CTabs>`,
 });
+
+Overview.args = {
+  labels: ['Tab 1', 'Tab 2', 'Tab 3'],
+};
+
+LabelSlots.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Check that labels array is the same length as the number of visible tabs
+  const tabs = await screen.findByRole('tablist');
+  // Check that the all tabs are visible and match the labels prop supplied
+  for (const tab of tabs.children) {
+    const label = tab.innerText;
+    // Check that the tab on screen is visible
+    const htmlTab = await screen.findByText(label);
+    expect(htmlTab).toBe(tab);
+  }
+  // Click on the second tab and assert that panel is displayed
+  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 2' }));
+  await screen.findByText('Panel 2');
+  // Click on the third tab and assert that panel is displayed
+  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 3' }));
+  await screen.findByText('Panel 3');
+  // Click on the first tab and assert that panel is displayed
+  await userEvent.click(canvas.getByRole('tab', { name: 'Tab 1' }));
+  await screen.findByText('Panel 1');
+};
+
+Overview.play = async ({ args, canvasElement }) => {
+  // Use label slots test as base case for this test
+  await LabelSlots.play({ canvasElement });
+
+  // Check that labels array is the same length as the number of visible tabs, using args (props) passed in
+  const tabs = await screen.findByRole('tablist');
+  const numberOfTabs = tabs.children.length;
+  const numberOfLabelsArg = args.labels.length;
+  expect(numberOfTabs).toBe(numberOfLabelsArg);
+};
