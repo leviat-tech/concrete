7a9c71640b9e08f614534e2b6e0829a0165dd949
Dan Storey <danielstorey@gmail.com>
2022-10-13 11:16:06 +0100

    feat: make tabs component controllable

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -1,13 +1,19 @@
 <template>
-  <TabGroup as="div">
+  <TabGroup as="div" class="h-full flex flex-col"
+            :selectedIndex="modelValue"
+            :defaultIndex="defaultIndex"
+            @change="onChange">
+
     <CTabList v-if="tabPosition === 'top'" class="border-b" :labels="labels">
       <!-- CTabLabel must be used in the template labels slot -->
       <slot name="labels" />
     </CTabList>
-    <TabPanels class="flex h-full">
-      <!-- CTabpanel must be used in the template panels slot -->
-      <slot name="panels" />
+
+    <TabPanels class="flex flex-col flex grow">
+      <!-- CTabPanel must be used in the default slot -->
+      <slot />
     </TabPanels>
+
     <CTabList v-if="tabPosition === 'bottom'" :labels="labels" class="border-t">
       <!-- CTabLabel must be used in the template labels slot -->
       <slot name="labels" />
@@ -18,6 +24,7 @@
 <script setup>
 import { TabGroup, TabPanels } from '@headlessui/vue';
 import CTabList from './CTabList.vue';
+import { ref } from 'vue';
 
 const props = defineProps({
   labels: { type: Array },
@@ -28,5 +35,22 @@ const props = defineProps({
       return ['top', 'bottom'].includes(value);
     },
   },
+  defaultIndex: { type: Number, default: 0 },
+  modelValue: { type: Number }
 });
+
+const emit = defineEmits(['update:modelValue'])
+
+const useVModel = props.modelValue !== undefined;
+
+const selectedIndex = (useVModel) ? props.modelValue : ref(0);
+
+function onChange(index) {
+  if (useVModel) {
+    emit('update:modelValue', index);
+  } else {
+    selectedIndex.value = index;
+  }
+}
+
 </script>
