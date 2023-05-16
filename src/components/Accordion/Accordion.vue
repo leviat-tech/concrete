e263731f18b7dbd458598f8ba4cbcd63103fcaec
reeves999999 <55240533+reeves999999@users.noreply.github.com>
2023-05-16 15:59:24 +0100

    chore: revised accordion

--- src/components/Accordion/Accordion.vue
+++ src/components/Accordion/Accordion.vue
@@ -62,8 +62,9 @@ let isOpen = accordionState[props.accordionId] ?? props.defaultOpen;
 const emit = defineEmits(['opened', 'closed']);
 
 const initialHeight = isOpen ? 'auto' : 0;
+const initialOverflow = isOpen ? 'visible' : 'hidden';
 const height = ref(initialHeight);
-let styles = reactive({ height: initialHeight, overflow: 'hidden' });
+let styles = reactive({ height: initialHeight, overflow: initialOverflow });
 const content = ref(null);
 const TRANSITION_TIME = props.transition ? 400 : 0;
 
