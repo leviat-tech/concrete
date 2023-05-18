b8d4ebeb49c9a668060ed8d9cdc3c3503d86fbf5
reeves999999 <55240533+reeves999999@users.noreply.github.com>
2023-05-18 15:34:29 +0100

    chore: PR comments applied

--- src/components/Listbox/Listbox.vue
+++ src/components/Listbox/Listbox.vue
@@ -67,7 +67,7 @@
 
         <transition
           enter-from-class="transition opacity-0 duration-150 ease-in -translate-y-6"
-          leave-to-class="transition opacity-0 duration-150 ease-in -translate-y-6"
+          leave-to-class="transition-opacity opacity-0 duration-150"
           enter-to-class="ease-in duration-300 ease-out opacity-100 translate-y-0"
           name="listbox"
         >
