844b1d8f150b5ecfe3ed85175f72acd5cdabe442
reeves999999 <55240533+reeves999999@users.noreply.github.com>
2023-05-22 16:08:37 +0100

    fix: trivial change to trigger release

--- src/components/Listbox/Listbox.vue
+++ src/components/Listbox/Listbox.vue
@@ -19,6 +19,7 @@
       :disabled="isDisabled"
       :multiple="multiple"
       v-slot="{ open }"
+      class="concrete__listbox"
     >
       <div :class="['relative', disabledClass]">
         <div class="inline-flex w-full">
