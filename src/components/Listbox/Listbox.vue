ca8fb65099d70d1f79cfe846a81c696c3f52cdb1
reeves999999 <55240533+reeves999999@users.noreply.github.com>
2023-05-22 11:00:58 +0100

    feat: add release-please (trivial change added)

--- src/components/Listbox/Listbox.vue
+++ src/components/Listbox/Listbox.vue
@@ -19,7 +19,7 @@
       :disabled="isDisabled"
       :multiple="multiple"
       v-slot="{ open }"
-      class="concrete__listbox aaa"
+      class="concrete__listbox release-please-setup"
     >
       <div :class="['relative', disabledClass]">
         <div class="inline-flex w-full">
