a8a8168b0478f28b15b8be22029061160697e5be
kwasilonek <87523973+kwasilonek@users.noreply.github.com>
2024-10-02 09:11:48 +0200

    fix: add tbody to table (#307)
    Co-authored-by: Kamil Wasilonek <kamilwasilonek@griddynamics.com>

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -1,118 +1,121 @@
 <template>
   <div class="flex flex-col w-full overflow-x-auto">
     <table class="table-auto" :class="tableClass">
-      <tr class="text-left border-b text-xs text-gray-600">
-        <th v-if="$slots.prepend" :class="prependClass" />
-        <th
-          v-for="col in columns"
-          :key="col.id"
-          :class="headerClass || 'pb-3 pl-1'"
-        >
-          <span
-            :class="{ 'cursor-pointer': col.sortable }"
-            @click="() => col.sortable && toggleSort(col.id)"
+      <tbody>
+        <tr class="text-left border-b text-xs text-gray-600">
+          <th v-if="$slots.prepend" :class="prependClass" />
+          <th
+            v-for="col in columns"
+            :key="col.id"
+            :class="headerClass || 'pb-3 pl-1'"
           >
-            <slot :name="`${col.id}.header`">
-              <span v-if="col.label?.html" v-html="col.label?.html"></span>
-              <span v-else :class="col.label || 'uppercase'">
-                {{ col.label || startCase(col.id) }}
-              </span>
-            </slot>
-          </span>
-          <span v-if="localSort[col.id] === 'asc'"> &#8593; </span>
-          <span v-else-if="localSort[col.id] === 'desc'"> &#8595; </span>
-          <span v-else-if="col.sortable" class="text-gray-400"> &#8645; </span>
-        </th>
-        <th v-if="attrs.onEdit || attrs.onAdd" class="w-12" />
-        <th v-if="attrs.onEdit || attrs.onAdd || attrs.onDelete" class="w-12" />
-        <th v-if="$slots.append" :class="appendClass" />
-      </tr>
-
-      <tr
-        v-for="(row, i) in _rows"
-        :key="`row${i}`"
-        :class="[
-          attrs.onClick && editingRow?._index !== i && 'cursor-pointer',
-          editingRow?._index === i && 'bg-gray-100',
-          rowClass || 'border-b',
-        ]"
-        @click="editingRow?._index !== i && emit('click', row)"
-      >
-        <td v-if="$slots.prepend">
-          <slot name="prepend" v-bind="row" />
-        </td>
-
-        <td
-          v-for="(col, j) in columns"
-          :class="cellClass || 'py-3 px-2'"
-          :key="`col${j}`"
-        >
-          <slot
-            :name="col.id"
-            :value="editingRow?._index === i ? editingRow[col.id] : row[col.id]"
-            @edit="editingRow[col.id] = $event"
-            :is-editing="editingRow?._index === i"
-            :error="(editingRow?._index === i && errors[col.id]) || undefined"
-            :row="row"
-          >
-            {{ col?.formatter?.(row[col.id], row) || get(row, col.id) }}
-          </slot>
-        </td>
-
-        <td v-if="editingRow?._index === i">
-          <span class="cursor-pointer" @click.stop="saveEdit">
-            <c-icon type="save" size="sm" />
-          </span>
-        </td>
-        <td v-else-if="attrs.onEdit && editingRow?.index !== i">
-          <span class="cursor-pointer" @click.stop="beginEditingRow(row, i)">
-            <c-icon type="edit" size="sm" />
-          </span>
-        </td>
-
-        <td v-if="editingRow?._index === i">
-          <span class="cursor-pointer" @click.stop="editingRow = null">
-            <c-icon type="cancel" size="sm" />
-          </span>
-        </td>
-
-        <td v-else-if="attrs.onDelete">
-          <div class="flex items-center">
-            <span class="cursor-pointer" @click.stop="emit('delete', row)">
-              <c-icon type="trash" size="sm" />
+            <span
+              :class="{ 'cursor-pointer': col.sortable }"
+              @click="() => col.sortable && toggleSort(col.id)"
+            >
+              <slot :name="`${col.id}.header`">
+                <span v-if="col.label?.html" v-html="col.label?.html"></span>
+                <span v-else :class="col.label || 'uppercase'">
+                  {{ col.label || startCase(col.id) }}
+                </span>
+              </slot>
             </span>
-          </div>
-        </td>
+            <span v-if="localSort[col.id] === 'asc'"> &#8593; </span>
+            <span v-else-if="localSort[col.id] === 'desc'"> &#8595; </span>
+            <span v-else-if="col.sortable" class="text-gray-400"> &#8645; </span>
+          </th>
+          <th v-if="attrs.onEdit || attrs.onAdd" class="w-12" />
+          <th v-if="attrs.onEdit || attrs.onAdd || attrs.onDelete" class="w-12" />
+          <th v-if="$slots.append" :class="appendClass" />
+        </tr>
 
-        <td v-if="$slots.append">
-          <slot name="append" v-bind="row" />
-        </td>
-      </tr>
+        <tr
+          v-for="(row, i) in _rows"
+          :key="`row${i}`"
+          :class="[
+            attrs.onClick && editingRow?._index !== i && 'cursor-pointer',
+            editingRow?._index === i && 'bg-gray-100',
+            rowClass || 'border-b',
+          ]"
+          @click="editingRow?._index !== i && emit('click', row)"
+        >
+          <td v-if="$slots.prepend">
+            <slot name="prepend" v-bind="row" />
+          </td>
+  
+          <td
+            v-for="(col, j) in columns"
+            :class="cellClass || 'py-3 px-2'"
+            :key="`col${j}`"
+          >
+            <slot
+              :name="col.id"
+              :value="editingRow?._index === i ? editingRow[col.id] : row[col.id]"
+              @edit="editingRow[col.id] = $event"
+              :is-editing="editingRow?._index === i"
+              :error="(editingRow?._index === i && errors[col.id]) || undefined"
+              :row="row"
+            >
+              {{ col?.formatter?.(row[col.id], row) || get(row, col.id) }}
+            </slot>
+          </td>
+  
+          <td v-if="editingRow?._index === i">
+            <span class="cursor-pointer" @click.stop="saveEdit">
+              <c-icon type="save" size="sm" />
+            </span>
+          </td>
+          <td v-else-if="attrs.onEdit && editingRow?.index !== i">
+            <span class="cursor-pointer" @click.stop="beginEditingRow(row, i)">
+              <c-icon type="edit" size="sm" />
+            </span>
+          </td>
+  
+          <td v-if="editingRow?._index === i">
+            <span class="cursor-pointer" @click.stop="editingRow = null">
+              <c-icon type="cancel" size="sm" />
+            </span>
+          </td>
+  
+          <td v-else-if="attrs.onDelete">
+            <div class="flex items-center">
+              <span class="cursor-pointer" @click.stop="emit('delete', row)">
+                <c-icon type="trash" size="sm" />
+              </span>
+            </div>
+          </td>
+  
+          <td v-if="$slots.append">
+            <slot name="append" v-bind="row" />
+          </td>
+        </tr>
 
-      <tr v-if="addingRow" class="border-b bg-gray-100">
-        <td v-if="$slots.prepend" />
+        <tr v-if="addingRow" class="border-b bg-gray-100">
+          <td v-if="$slots.prepend" />
+  
+          <td v-for="(col, j) in columns" :key="`col${j}`" class="py-3 pl-1">
+            <slot
+              :name="col.id"
+              :value="addingRow[col.id]"
+              :isEditing="true"
+              @edit="addingRow[col.id] = $event"
+              :error="(addingRow && errors[col.id]) || undefined"
+              :row="addingRow"
+            />
+          </td>
+          <td>
+            <span @click="saveAdd" class="cursor-pointer">
+              <c-icon type="save" size="sm" />
+            </span>
+          </td>
+          <td>
+            <span @click="addingRow = null" class="cursor-pointer">
+              <c-icon type="cancel" size="sm" />
+            </span>
+          </td>
+        </tr>
 
-        <td v-for="(col, j) in columns" :key="`col${j}`" class="py-3 pl-1">
-          <slot
-            :name="col.id"
-            :value="addingRow[col.id]"
-            :isEditing="true"
-            @edit="addingRow[col.id] = $event"
-            :error="(addingRow && errors[col.id]) || undefined"
-            :row="addingRow"
-          />
-        </td>
-        <td>
-          <span @click="saveAdd" class="cursor-pointer">
-            <c-icon type="save" size="sm" />
-          </span>
-        </td>
-        <td>
-          <span @click="addingRow = null" class="cursor-pointer">
-            <c-icon type="cancel" size="sm" />
-          </span>
-        </td>
-      </tr>
+      </tbody>
     </table>
     <div
       v-if="attrs.onAdd && !addingRow"
