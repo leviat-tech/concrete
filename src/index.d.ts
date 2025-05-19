b0f9d2e981fe256ba8de7387a31ad92b670ffc88
kwasilonek <87523973+kwasilonek@users.noreply.github.com>
2025-05-19 10:08:59 +0200

    fix: types for common components (#335)
    Co-authored-by: Kamil Wasilonek <kamilwasilonek@griddynamics.com>

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,23 @@
+import type CButton from './components/Button/Button.vue';
+import type CListbox from './components/Listbox/Listbox.vue';
+import type CCheckbox from './components/Checkbox/Checkbox.vue';
+import type CPopupMenu from './components/PopupMenu/PopupMenu.vue';
+import type CFromSection from './components/FormSection/FormSection.vue';
+import type CNumericInput from './components/NumericInput/NumericInput.vue';
+import type CStatusIndicator from './components/Status/StatusIndicator.vue';
+import type CConfirmationModal from './components/Modal/ConfirmationModal.vue';
+import type CSelectFileType from './components/SelectFileType/SelectFileType.vue';
 
-import { ComponentCustomProperties } from 'vue';
-
-declare module '@vue/runtime-core' {
-    interface ComponentCustomProperties {
-        $L: (value: string, params?: { capitalize?: boolean }) => string;
-        $l: (value: string, params?: { capitalize?: boolean }) => string;
-    }
+declare module 'vue' {
+  export interface GlobalComponents {
+    CButton: typeof CButton;
+    CListbox: typeof CListbox;
+    CCheckbox: typeof CCheckbox;
+    CPopupMenu: typeof CPopupMenu;
+    CFormSection: typeof CFromSection;
+    CNumericInput: typeof CNumericInput;
+    CSelectFileType: typeof CSelectFileType;
+    CStatusIndicator: typeof CStatusIndicator;
+    CConfirmationModal: typeof CConfirmationModal;
+  }
 }
-
